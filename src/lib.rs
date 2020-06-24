057205d5228a8d87789bd5f9b2108e5596ce5845
ugnic <ryuto.nishida@meetasu.jp>
2020-06-24 17:41:50 +0900

    m

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
+use std::fs;
 
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn opFi(path: &str) {
+    open(path);
+}
+
+fn open(path: &str)-> Result<(), Box<std::error::Error>> {
+    let content = fs::read_to_string(path)?;
+    alert(&format!("{}", content));
+    Ok(())
 }
 
 #[wasm_bindgen]
